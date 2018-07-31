ba9b7a5fa09a489701ced6e84f24b0c48a7d1864
Ruben Ribeiro @ Home <ruben.d.ribeiro@outlook.pt>
2018-07-31 15:17:45 +0000

    Import DB

---
+++ Database/WarehouseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcademiadecodigoWarehouseApi.Database
+{
+    public class WarehouseContext : DbContext
+    {
+        public WarehouseContext(DbContextOptions<WarehouseContext> options) : base(options)
+        {
+
+        }
+    }
+}
