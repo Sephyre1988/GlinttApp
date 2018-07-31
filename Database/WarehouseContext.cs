570f7f30ebb093032774148a64ce53d629cd858b
Ruben Ribeiro @ Home <ruben.d.ribeiro@outlook.pt>
2018-07-31 17:05:40 +0000

    Db Progress

--- Database/WarehouseContext.cs
+++ Database/WarehouseContext.cs
@@ -12,5 +12,52 @@ namespace AcademiadecodigoWarehouseApi.Database
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductEntity>(cfg =>
+            {
+                cfg.ToTable("Products");
+
+                cfg.HasKey(e => e.Id);
+                cfg.HasIndex(e => e.Code)
+                   .IsUnique(true);
+                cfg.HasIndex(e => e.Name).IsUnique(true);
+
+                cfg.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+                cfg.Property(e => e.Code).IsRequired().HasMaxLength(3);
+                cfg.Property(e => e.Name).IsRequired().HasMaxLength(20);
+                cfg.Property(e => e.Description).HasMaxLength(1024);
+                cfg.Property(e => e.Price).IsRequired();
+                cfg.Property(e => e.Code).IsRequired().HasMaxLength(3);
+
+                cfg.Property(e => e.CreatedBy).IsRequired().HasMaxLength(128);
+                cfg.Property(e => e.CreatedOn).IsRequired();
+                cfg.Property(e => e.UpdatedBy).IsRequired().HasMaxLength(128);
+                cfg.Property(e => e.UpdatedOn).IsRequired();
+                cfg.Property(e => e.DeletedBy).HasMaxLength(128);
+                cfg.Property(e => e.DeletedOn);
+
+                cfg.Property(e => e.Version).IsRequired().IsConcurrencyToken();
+
+                cfg.HasMany(e => e.StockMoviments).WithOne(e => e.Product)
+                    .HasForeignKey(e=> e.ProductId).IsRequired();
+            });
+
+            modelBuilder.Entity<StockMovimentEntity>(cfg =>
+            {
+                cfg.ToTable("StockMovements");
+                cfg.HasKey(e => e.Id);
+                cfg.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+                cfg.Property(e => e.Price).IsRequired();
+                cfg.Property(e => e.Quantity).IsRequired();
+
+                cfg.Property(e => e.CreatedBy).IsRequired().HasMaxLength(128);
+                cfg.Property(e => e.CreatedOn).IsRequired();
+
+                cfg.HasOne(e => e.Product).WithMany(e => e.StockMoviments).HasForeignKey(e => e.ProductId).IsRequired();
+
+            });
+        }
     }
 }
